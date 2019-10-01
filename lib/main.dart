073ff2cc3d22a2a43be835a5b04b0399998825f7
kapil jhajhria <jhajhria44@gmail.com>
2019-10-01 10:54:16 +0530

    Day 2: In class Ex Container, Column, Margin, Padding

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,42 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Container(),
-      ),
+      home: MyColor(),
     ),
   );
+}
+
+class MyColor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 100,
+              color: Colors.orange,
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 10.0),
+            ),
+            Container(
+              height: 100,
+              color: Colors.white,
+              child: Center(
+                child: Image.network(
+                  ('https://raw.githubusercontent.com/McLarenCollege/Flutter-Course-Notes/master/ensign.png'),
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              color: Colors.green,
+              margin: EdgeInsets.fromLTRB(0, 10.0, 0, 0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
